5e719bdbc948cb20c96299693650c5ae2d4dcb80
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-03-03 23:08:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  attr_accessor :roster, :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student student, grade
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+  def grade grade
+    @roster[grade]
+  end
+  def sort
+    @roster.each {|grade, _|
+      @roster[grade] = @roster[grade].sort
+    }
+  end
+end
